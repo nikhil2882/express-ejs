70842023196f369df3e5ce6f886f634cd2cebd8a
nikhil sengar <nikhil@farmart.co>
2021-11-09 09:20:43 +0530

    Second iteration

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 const express = require('express')
-const fs = require('fs')
 const app = express()
 const port = 3000
+
+var session = require('express-session');
+
+app.use(express.static("public"))
 
 app.set('view engine', 'ejs');
 //app.set('views', __dirname+"/abc");
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+}))
+
+app.use(express.urlencoded({ extended:true }));
+
 app.get('/', (req, res) => 
 {
-  const { name , age} = req.query;
+  const { username } = req.session;
 
-  res.render("home.ejs", { name: name, dob: 'xxxx-xx-xx', books:["harry potter","jungle book","abcd"] });
+  if(!username)
+  {
+    res.redirect("/login");
+    return
+  }
+
+  res.render("home", { name: username, books: ["jungle book", "harry potter", "0 to 1" , "the secret"] });
+})
+
+
+
+app.get("/login", function(req, res)
+{
+  res.sendFile(__dirname+"/views/login.html");
+})
+
+app.post("/login", function(req, res)
+{
+  const { username } = req.body;
+  //const username = req.body.username;
+
+  req.session.username = username;
+
+  res.redirect("/")
 })
 
 app.listen(port, () => {
